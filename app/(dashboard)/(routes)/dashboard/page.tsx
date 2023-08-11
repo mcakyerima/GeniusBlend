b056be7b3fe34b2faa28c788de6800fca766b270
mcakyerima <mcakyerima@gmail.com>
2023-08-11 22:08:27 +0100

    first-commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+const DashboardPage = () => {
+  return (
+    <h2>This is the Dashboard</h2>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing_page)/page.tsx
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+    return ( 
+        <h2>THIS IS LANDING PAGE</h2>
+     );
+}
+ 
+export default LandingPage;
